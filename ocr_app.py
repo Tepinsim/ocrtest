8fbde2854612319cde752c8bb3506127bf4d4eb7
Charmzz <118677880+Tepinsim@users.noreply.github.com>
2025-03-05 17:02:14 +0700

    first commit

---
+++ ocr_app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import torch
+from PIL import Image
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+
+
+# Load model and processor (replace with your model path)
+model_path = "/Users/Cs-Store/Desktop/intern2/OCRME/fine_tuned_trocr_khmer"
+processor = TrOCRProcessor.from_pretrained(model_path)
+model = VisionEncoderDecoderModel.from_pretrained(model_path)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+model.eval()
+
+def perform_ocr(image):
+    """Performs OCR on the given image."""
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values.to(device)
+    with torch.no_grad():
+        generated_ids = model.generate(pixel_values)
+    predicted_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return predicted_text
+
+# Streamlit UI
+st.title("Khmer OCR with TrOCR")
+
+uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.write("")
+    st.write("Prediction:")
+    predicted_text = perform_ocr(image)
+    st.write(predicted_text)
