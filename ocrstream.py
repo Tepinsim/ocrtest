d9257389eb01b0258d429d920cd463e4e76f3a88
Charmzz <118677880+Tepinsim@users.noreply.github.com>
2025-03-05 19:18:07 +0700

    Update ocrstream.py

--- ocrstream.py
+++ ocrstream.py
@@ -2,9 +2,11 @@ import streamlit as st
 import torch
 from PIL import Image
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+#from google.colab import drive #Remove if not running in colab.
 
-# Load your model (replace with your actual model path)
-model_path = "/Users/Cs-Store/Desktop/intern2/OCRME/fine_tuned_trocr_khmer"  
+
+# Load your model (replace with your model path)
+model_path = "/Users/Cs-Store/Desktop/intern2/OCRME/fine_tuned_trocr_khmer"
 processor = TrOCRProcessor.from_pretrained(model_path)
 model = VisionEncoderDecoderModel.from_pretrained(model_path)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -19,19 +21,16 @@ def perform_ocr(image):
     predicted_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
     return predicted_text
 
-# Streamlit UI
 st.title("Khmer OCR with Feedback")
 
-uploaded_file = st.file_uploader("Upload an image", type=["png"])  # PNG only for now
+uploaded_file = st.file_uploader("Upload an image", type=["png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
     predicted_text = perform_ocr(image)
-
-    # Display prediction with Khmer font
-    st.markdown(f'<p style="font-family: Khmer OS Battambang;">**Prediction:** {predicted_text}</p>', unsafe_allow_html=True)
+    st.write(f"**Prediction:** {predicted_text}")
 
     col1, col2 = st.columns(2)
     with col1:
